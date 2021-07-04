148a280e616d9689d156cd0964674421cef68aaa
Vasili Kachalko <amarant.st@gmail.com>
2021-07-04 12:40:16 +0200

    add vlc api

---
+++ button_blink_vlc.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rpi_gpio'
+
+
+class ButtonBlinkVlc
+  LED_PIN = 11
+  BUTTON_PIN = 12
+
+  attr_reader :vlc
+
+  def run
+    setup
+    first_time = true
+
+    while true do
+      if RPi::GPIO.low? BUTTON_PIN
+        unless vlc.playing?
+          if first_time
+            vlc.play("/home/pi/Movies/rick.avi")
+          else
+            vlc.play
+          end
+        end
+
+        RPi::GPIO.set_high LED_PIN
+
+      else
+        vlc.pause
+        RPi::GPIO.set_low LED_PIN
+      end
+    end
+  ensure
+    RPi::GPIO.clean_up
+  end
+
+  private
+
+  def setup
+    vlc = VLC::System.new
+    RPi::GPIO.set_numbering :board
+    RPi::GPIO.setup LED_PIN, as: :output, initialize: :low
+    RPi::GPIO.setup BUTTON_PIN, as: :input, pull: :up
+  end
+end
+
+ButtonBlinkVlc.new.run
