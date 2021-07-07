a02f437939c3a7011f017c06d72c47aade9f1990
Vasili Kachalko <amarant.st@gmail.com>
2021-07-07 20:59:32 +0200

    add basic temp/humidity sensor reader

---
+++ temp_humidity.rb
@@ -0,0 +1,179 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'rpi_gpio'
+
+class TempHumidity
+  DHTPin = 11
+
+  class DHT
+	  DHTLIB_OK             = 0
+	  DHTLIB_ERROR_CHECKSUM = -1
+	  DHTLIB_ERROR_TIMEOUT  = -2
+	  DHTLIB_INVALID_VALUE  = -999
+
+	  DHTLIB_DHT11_WAKEUP   = 0.020  #20ms
+	  DHTLIB_TIMEOUT        = 0.0001 #100us
+
+    attr_reader :humidity, :temperature, :pin
+    attr_accessor :pin, :bits
+
+    def initialize(pin)
+      @humidity = 0
+      @temperature = 0
+      @pin = pin
+		  self.bits = [0,0,0,0,0]
+		  RPi::GPIO.set_numbering :board
+    end
+
+
+	  def readSensor(wakeup_delay)
+		  mask = 0x80
+		  idx = 0
+		  self.bits = [0,0,0,0,0]
+
+		  # Clear sda
+      RPi::GPIO.setup pin, as: :output, initialize: :high
+
+		  sleep 0.5
+
+		  # start signal
+      RPi::GPIO.set_low pin
+		  sleep(DHTLIB_DHT11_WAKEUP)
+		  RPi::GPIO.set_high pin
+		  # time.sleep(0.000001)
+      RPi::GPIO.setup pin, as: :input
+
+		  # Waiting echo
+		  t = Time.current
+
+		  while true
+			  if RPi::GPIO.low?(pin)
+				  break
+        end
+			  if dht_timeout?(t)
+				  return DHTLIB_ERROR_TIMEOUT
+        end
+      end
+
+		  # Waiting echo low level end
+		  t = Time.current
+
+ 		  while RPi::GPIO.low?(pin)
+			  if dht_timeout?(t)
+				  return DHTLIB_ERROR_TIMEOUT
+        end
+      end
+
+		  # Waiting echo high level end
+		  t = Time.current
+
+ 		  while RPi::GPIO.high?(pin)
+			  if dht_timeout?(t)
+				  return DHTLIB_ERROR_TIMEOUT
+        end
+      end
+
+		  for i in range(0,40,1)
+		    t = Time.current
+
+			  while RPi::GPIO.low?(pin)
+				  if dht_timeout?(t)
+					  #print ("Data Low %d"%(i))
+					  return DHTLIB_ERROR_TIMEOUT
+          end
+        end
+
+			  t = Time.current
+
+			  while RPi::GPIO.high?(pin)
+				  if dht_timeout?(t)
+					  #print ("Data HIGH %d"%(i))
+					  return DHTLIB_ERROR_TIMEOUT
+          end
+        end
+
+			  if (Time.current - t) > 0.00005
+				  self.bits[idx] |= mask
+        end
+
+			  mask >>= 1
+
+			  if mask == 0
+				  mask = 0x80
+				  idx += 1
+        end
+      end
+
+
+		  # time.sleep(0.000001)
+
+      RPi::GPIO.setup pin, as: :output, initialize: :high
+
+      return DHTLIB_OK
+    end
+
+	  #Read DHT sensor, analyze the data of temperature and humidity
+	  def readDHT11Once
+		  rv = readSensor
+		  if rv != DHTLIB_OK
+			  @humidity = DHTLIB_INVALID_VALUE
+			  @temperature = DHTLIB_INVALID_VALUE
+			  return rv
+      end
+
+		  @humidity = bits[0]
+		  @temperature = bits[2] + bits[3]*0.1
+		  sumcheck = (bits[0] + bits[1] + bits[2] + bits[3]) & 0xFF
+
+		  if bits[4] != sumcheck
+			  return DHTLIB_ERROR_CHECKSUM
+      end
+
+		  return DHTLIB_OK
+
+	def readDHT11
+		result = DHTLIB_INVALID_VALUE
+
+		for i in range(0,15)
+			result = readDHT11Once
+			if result == DHTLIB_OK
+				return DHTLIB_OK
+      end
+
+			sleep(0.1)
+    end
+
+		return result
+  end
+
+  def run
+    setup
+
+    while true
+      readDHT11
+
+      puts "Temperature: #{temperature}"
+      puts "Humidity #{humidity}"
+
+      sleep 60
+    end
+  ensure
+    RPi::GPIO.clean_up
+  end
+
+
+  def setup
+    RPi::GPIO.set_numbering :board
+    RPi::GPIO.setup R_PIN, as: :output, initialize: :low
+    RPi::GPIO.setup G_PIN, as: :output, initialize: :low
+    RPi::GPIO.setup B_PIN, as: :output, initialize: :low
+  end
+
+  private
+
+  def dht_timeout?(t)
+    (Time.current - t) > DHTLIB_TIMEOUT
+  end
+end
+
+TempHumidity.new.run
